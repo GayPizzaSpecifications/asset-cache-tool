52ac62a6756b66d3fd623612fffc7d245a99c582
Kenneth Endfinger <kaendfinger@gmail.com>
2021-10-25 00:04:33 -0400

    Add support for sourceScheme while downloading assets from a remote cache.

--- Sources/AssetCacheLibrary/RemoteAssetCache.swift
+++ Sources/AssetCacheLibrary/RemoteAssetCache.swift
@@ -15,7 +15,7 @@ public struct RemoteAssetCache {
     }
 
     public func download(_ originFileURL: URL, to targetFileURL: URL) throws {
-        let request = URLRequest(url: downloadURL(for: originFileURL))
+        let request = URLRequest(url: try downloadURL(for: originFileURL))
         let (downloadedFileURL, _, error) = URLSession.shared.synchronousDownloadTask(with: request)
         if error != nil {
             throw error!
@@ -25,19 +25,34 @@ public struct RemoteAssetCache {
             throw URLError(URLError.badServerResponse)
         }
 
+        if FileManager.default.fileExists(atPath: targetFileURL.path) {
+            try FileManager.default.removeItem(at: targetFileURL)
+        }
         try FileManager.default.moveItem(at: downloadedFileURL!, to: targetFileURL)
     }
 
-    public func downloadURL(for originFileURL: URL) -> URL {
+    public func downloadURL(for originFileURL: URL) throws -> URL {
         var cacheServerURLComponents = URLComponents(string: originFileURL.absoluteString)!
         let host = cacheServerURLComponents.host!
+        let scheme = cacheServerURLComponents.scheme!
+        let port: Int
+        if cacheServerURLComponents.port != nil {
+            port = cacheServerURLComponents.port!
+        } else if originFileURL.scheme == "http" {
+            port = 80
+        } else if originFileURL.scheme == "https" {
+            port = 443
+        } else {
+            throw URLError(URLError.badURL)
+        }
         cacheServerURLComponents.scheme = "http"
         cacheServerURLComponents.port = serverBaseURL.port
         cacheServerURLComponents.host = serverBaseURL.host
         if cacheServerURLComponents.queryItems == nil {
             cacheServerURLComponents.queryItems = []
         }
-        cacheServerURLComponents.queryItems!.append(URLQueryItem(name: "source", value: host))
+        cacheServerURLComponents.queryItems!.append(URLQueryItem(name: "source", value: "\(host):\(port)"))
+        cacheServerURLComponents.queryItems!.append(URLQueryItem(name: "sourceScheme", value: scheme))
         return cacheServerURLComponents.url!
     }
 }
